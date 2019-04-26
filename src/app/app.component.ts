b61d3e4ced275559d1878f50aae7766155e73b66
aatish810 <aatish.sharma28@gamil.com>
2019-04-27 03:10:49 +0530

    test 1 with reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,11 @@ import * as lodash from 'lodash';
 })
 export class AppComponent implements OnInit {
   title = 'ignite-test';
-  private filesControl = new FormControl(null, FileUploadValidators.accept(['file_extension']));
+  private filesControl = new FormControl(null);
   public demoForm: FormGroup;
   public invalidFileSizeDetail = [];
   public invalidFileFormatDetail = [];
-
+  public copyArray = [];
   constructor(private formBuilder: FormBuilder) {
   }
 
@@ -26,47 +26,48 @@ export class AppComponent implements OnInit {
   }
 
   onChanges() {
-    // this.demoForm.get('files').valueChanges.subscribe(() => {
-    //   let count = 0;
-    //   for (let file of this.demoForm.get('files').value) {
-    //     if (file.name) {
-    //       let fileExtn = this.getFileExtension(file.name);
-    //       // checking file format
-    //       if (fileExtn != 'png' && fileExtn != 'jpg' && fileExtn != 'pdf') {
-    //         if (!lodash.includes(this.invalidFileFormatDetail, file.name)) {
-    //           this.invalidFileFormatDetail.push(file.name);
-    //           alert('not a valid file');
-    //         }
-    //         this.removeInValidFile(file);
-    //       } else {
-    //       // checking file size
-    //       let fileSize = file.size / 1024 / 1024;
-    //       if (fileSize > 5) {
-    //         console.log(lodash.includes(this.invalidFileSizeDetail, file.name))
-    //         if (!lodash.includes(this.invalidFileSizeDetail, file.name)) {
-    //           this.invalidFileSizeDetail.push(file.name);
-    //           alert('only 5 MB is allowed');
-    //         }
-    //         this.removeInValidFile(file);
-    //       }
-    //       }
-    //     }
-    //     if (this.demoForm.get('files').value.length >= 5) {
-    //       this.demoForm.disable();
-    //     }
-    //   }
+    this.demoForm.get('files').valueChanges.subscribe(() => {
+      let count = 0;
+      for (let file of this.demoForm.get('files').value) {
+        if (file.name) {
 
-    // });
+          let fileExtn = this.getFileExtension(file.name);
+          // checking file format
+          if (fileExtn === 'png' || fileExtn === 'jpg' || fileExtn === 'pdf') { 
+            let fileSize = file.size / 1024 / 1024;
+            if (fileSize > 5) {
+              if (!lodash.includes(this.invalidFileSizeDetail, file.name)) {
+                this.invalidFileSizeDetail.push(file.name);
+                alert('only 5 MB is allowed');
+              }
+              this.removeInValidFile(file);
+            }
+          } else {
+            if (!lodash.includes(this.invalidFileFormatDetail, file.name)) {
+              this.invalidFileFormatDetail.push(file.name);
+              alert('not a valid file');
+            }
+            this.removeInValidFile(file);
+          }
+        }
+        if (this.demoForm.get('files').value.length >= 5) {
+          this.demoForm.disable();
+        }
+      }
+
+    });
   }
 
   removeInValidFile(file) {
+   // this.filesControl.setValue([]);
+   // console.log(this.filesControl.get('files'))
     if (this.demoForm.value.files === null) {
       this.filesControl.setValue([]);
     } else {
       lodash.remove(this.demoForm.get('files').value, (f) => {
         return f.name === file.name;
       });
-    }
+     }
   }
 
   getFileExtension(name) {
