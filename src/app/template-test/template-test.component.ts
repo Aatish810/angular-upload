88d956e6c5afec0bcf9a5d42933fd7811b2cbd2a
aatish810 <aatish.sharma28@gamil.com>
2019-04-27 14:33:19 +0530

    adding template test 1

--- src/app/template-test/template-test.component.ts
+++ src/app/template-test/template-test.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import * as lodash from 'lodash';
 
 @Component({
   selector: 'app-template-test',
@@ -7,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateTestComponent implements OnInit {
 
+  public uploadedFiles: Array<File> = [];
+  @ViewChild('demoForm') ngForm: NgForm;
+  private formChangesSubscription;
+  private invalidFileSizeDetail = [];
+  private invalidFileFormatDetail = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.formChangesSubscription = this.ngForm.form.valueChanges.subscribe(currentFiles => {
+     // console.log(files)
+      for(let file of currentFiles.files) {
+        let fileExtn = this.getFileExtension(file.name);
+        if(fileExtn === 'pdf' || fileExtn === 'jpg' || fileExtn === 'png') {
+          let fileSize = file.size / 1024 / 1024;
+          if (fileSize > 5) {
+            if (!lodash.includes(this.invalidFileSizeDetail, file.name)) {
+              this.invalidFileSizeDetail.push(file.name);
+              alert('only 5 MB is allowed');
+            }
+            this.removeInValidFile(file, currentFiles.files);
+          }
+        } else {
+          if (!lodash.includes(this.invalidFileFormatDetail, file.name)) {
+            this.invalidFileFormatDetail.push(file.name);
+            alert('not a valid file');
+          }
+          this.removeInValidFile(file, currentFiles.files);
+        }
+      }
+    });
   }
+  
+  ngOnDestroy() {
+    this.formChangesSubscription.unsubscribe();
+  }
+
+  getFileExtension(name) {
+    return name.split('.')[1];
+  }
+
+  removeInValidFile(file, files) {
+       lodash.remove(files, (f) => {
+         return f.name === file.name;
+       }); 
+   }
+
+   uploadFilesNow() {
+     console.log(this.uploadedFiles);
+   }
 
 }
